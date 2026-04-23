b857233893f66b485f1b6316dceb7bdf2e5dd81b
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2026-04-22 22:07:36 -0300

    Att

--- medical_plus/src/main/resources/static/js/cadastro.js
+++ medical_plus/src/main/resources/static/js/cadastro.js
@@ -2,3 +2,18 @@ window.addEventListener("pageshow", function () {
         document.querySelectorAll("input").forEach(input => input.value = "");
         document.querySelectorAll("select").forEach(select => select.selectedIndex = 0);
     });
+
+function toggleSenha(inputId, btn) {
+    const input = document.getElementById(inputId);
+    const eyeOn  = btn.querySelector('.icon-eye');
+    const eyeOff = btn.querySelector('.icon-eye-off');
+    if (input.type === 'password') {
+        input.type = 'text';
+        eyeOn.style.display  = 'none';
+        eyeOff.style.display = 'block';
+    } else {
+        input.type = 'password';
+        eyeOn.style.display  = 'block';
+        eyeOff.style.display = 'none';
+    }
+}
--- medical_plus/src/main/resources/static/js/login.js
+++ medical_plus/src/main/resources/static/js/login.js
@@ -1,3 +1,18 @@
 window.addEventListener("pageshow", function () {
         document.querySelectorAll("input").forEach(input => input.value = "");
     });
+
+function toggleSenha(inputId, btn) {
+    const input = document.getElementById(inputId);
+    const eyeOn  = btn.querySelector('.icon-eye');
+    const eyeOff = btn.querySelector('.icon-eye-off');
+    if (input.type === 'password') {
+        input.type = 'text';
+        eyeOn.style.display  = 'none';
+        eyeOff.style.display = 'block';
+    } else {
+        input.type = 'password';
+        eyeOn.style.display  = 'block';
+        eyeOff.style.display = 'none';
+    }
+}
